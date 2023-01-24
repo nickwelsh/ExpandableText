a4c44affc5ef3fe34c64cae8e5b9b1fc4dc3bb8c
Nick Welsh <nickwelsh@users.noreply.github.com>
2023-01-23 19:23:43 -0500

    macOS (will break things)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "ExpandableText",
     platforms: [
         .iOS(.v15),
-        .tvOS(.v15)
+        .tvOS(.v15),
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
